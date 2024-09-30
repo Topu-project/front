0a087f5b20f99fb471fdfd992a75e012399b3132
leemihyun <m.lee@cosmocomputing.co.jp>
2024-09-30 20:41:28 +0900

    fix: ログイン失敗画面作成

---
+++ src/app/login/retry/page.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import topuColors from "@/lib/colors";
+import { Box, Button, Divider, Stack, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import GoogleIcon from "../../../../public/icon/etc/system/GoogleIcon";
+import { useParams, useRouter, useSearchParams } from "next/navigation";
+import { useQuery } from "@tanstack/react-query";
+
+export default function LoginRetry() {
+  return (
+    <React.Fragment>
+      <Stack
+        sx={{
+          // width: "-webkit-fill-available",
+          // height: "-webkit-fill-available",
+          minHeight: "-webkit-fill-available",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          pt: "100px",
+        }}
+      >
+        <Box
+          sx={{
+            width: "580px",
+            height: "366px",
+            border: `6px solid ${topuColors.pointColor.purpleMain}`,
+            borderRadius: "20px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Box
+            sx={{
+              width: "500px",
+              height: "240px",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography fontSize={"30px"} fontWeight={"bold"}>
+              ログインに失敗しました。
+            </Typography>
+            <Typography fontSize={"18px"} textAlign={"center"}>
+              再度ログインしてください。
+            </Typography>
+            <Divider sx={{ width: "100%" }} />
+            <Button
+              href={`http://localhost:8080/auth/login`}
+              size="large"
+              variant="contained"
+              sx={{
+                backgroundColor: topuColors.grey.lightGrey,
+                color: topuColors.basic.black,
+                // border: `1px solid ${topuColors.pointColor.purpleMain}`,
+                fontSize: "12px",
+                minWidth: "225px",
+                maxHeight: "42.25px",
+                borderRadius: 40,
+                // ":hover": {
+                //   backgroundColor: "rgba(0, 0, 0, 0) !important",
+                //   boxShadow: "none !important",
+                //   border: `1px solid rgba(155, 39, 176, 0.6) !important`,
+                // },
+                // "&:active": {
+                //   backgroundColor: "rgba(0, 0, 0, 0) !important",
+                //   boxShadow: "none !important",
+                //   border: `1px solid rgba(155, 39, 176, 0.6) !important`,
+                // },
+              }}
+              startIcon={<GoogleIcon />}
+            >
+              Googleでログイン
+            </Button>
+          </Box>
+        </Box>
+      </Stack>
+    </React.Fragment>
+  );
+}
