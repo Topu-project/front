901dfedeafc98b8e1985abe345c696ab14eeb806
leemihyun <m.lee@cosmocomputing.co.jp>
2024-10-27 18:39:10 +0900

    fix: 1.login/loading pageを追加(googleログイン後のuser処理をここで判断) 2.signup userData 保存のためjotai install, userData 初期値設定

--- src/app/ThemeRegistry.tsx
+++ src/app/ThemeRegistry.tsx
@@ -3,6 +3,7 @@
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import theme from "./theme";
+import { UserProvider } from "@/providers/UserProvider";
 
 export default function ThemeRegistry({
   children,
@@ -11,8 +12,10 @@ export default function ThemeRegistry({
 }) {
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline />
-      {children}
+      <UserProvider>
+        <CssBaseline />
+        {children}
+      </UserProvider>
     </ThemeProvider>
   );
 }
