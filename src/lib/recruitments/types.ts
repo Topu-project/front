3011eec6cdcd9ea888ed0e5253bf1d061ea83b2b
leemihyun <m.lee@cosmocomputing.co.jp>
2024-10-27 19:07:11 +0900

    fix: main page カードデータ取得エラー対応

--- src/lib/recruitments/types.ts
+++ src/lib/recruitments/types.ts
@@ -1,33 +1,17 @@
-// 채용 정보 관련 타입 정의
-
-// 개별 채용 정보의 구조 정의
-// export interface Recruitment {
-//   id: string;
-//   recruitmentCategories: string;
-//   techStacks: string[];
-//   recruitmentPositions: string[];
-//   recruitmentDeadline: string;
-//   subject: string;
-// }
-
-export interface Recruitment {
-  id: string;
-  created_at: string;
-  updated_at: string;
-  recruitmentCategories: "STUDY" | "PROJECT" | "READING";
-  progressMethods: "ONLINE" | "OFFLINE" | "ALL";
+interface Recruitment {
+  id: number;
+  userId: number;
+  nickname: string;
+  recruitmentCategory: "STUDY" | "PROJECT" | "READING";
   techStacks: string[];
-  positions: string;
-  numberOfPeople: number;
-  progressPeriod: number;
+  positions: string[];
   recruitmentDeadline: string;
-  contract: string;
   subject: string;
-  content: string;
+  progressMethods: "ONLINE" | "OFFLINE" | "ALL";
+  views: number;
 }
 
-// API 응답의 구조 정의
-export interface RecruitmentResponse {
+interface RecruitmentResponse {
+  count: number;
   data: Recruitment[];
-  error: null | string;
 }
